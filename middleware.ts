4e55b81cb75eabb406c3536a2da96409e3fe7486
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-12-27 16:04:42 +0200

    route protection

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+// export { default } from "next-auth/middleware";
+
+// //Secures the matching routes...
+// export const config = { matcher: ["/"] };
+export { default } from "next-auth/middleware"
+
+export const config = { matcher: ["/"] }
